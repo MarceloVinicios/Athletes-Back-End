ad2b603561f6f7a7046ada6bec70f6066426654a
gmorais-dev1~ <guilherme_morais14@hotmail.com>
2023-08-30 09:48:20 -0300

    atualização de satus code

--- src/services/userService.js
+++ src/services/userService.js
@@ -2,10 +2,10 @@ class User_Service {
   async create() {
     const numero = Math.floor(Math.random() * (10 - 1 + 1)) + 1;
     if (numero > 5) {
-      return {statusCode: 400, response: "Sei lá"}
+      return {statusCode: 400, response: "teste de statuscode(erro)"}
     }
 
-    return {statusCode: 201, response: "yeah"}
+    return {statusCode: 201, response: "statuscode sucesso"}
   }
 }
 
