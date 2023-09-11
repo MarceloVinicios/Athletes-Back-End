ebf1b82bde3e8f11b0113325b317b59e1555e014
Ruan Ribeiro <Rrjalfim@gmail.com>
2023-09-11 19:42:10 -0300

    Sistema para pegar comentarios adicionado

--- src/services/commentService.js
+++ src/services/commentService.js
@@ -1,14 +1,39 @@
 const CommentModel = require("../models/CommentModel")
 
 class Comment_service {
-  async getAllComment(){
-  };
+  async getAllComment(req,res) {
+    try{
+      const responseGetAllComment = await CommentModel.getAll();
+      if (!responseGetAllComment.status) {
+        return {statusCode: 500, response: responseGetAllComment.msg}
+      }
+    }
+      if (responseGetAllComment.data.lenght > 0) {
 
-  async createComment(){
-  };
+      }
+    } 
+  }
+
+  async createComment(comment){
+    if(comment == undefined || comment ==""){
+      return { statusCode: 400, msg: 'Comment is required'};
+      
+    };
+
+    const responseCreate = await CommentModel.create(title, description);
+    if(!responseCreate.status) {
+      return { statusCode: 500, msg: responseCreate.err};
+    };
+  
+    return {status: true};
+  }
 
   async updateComment(){
+  
+  
+  
   };
+
 
   async deleteUserComment(){
   };
