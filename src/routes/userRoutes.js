c7cef8a35aaa9be1e49c2b4296436654f69e0442
gmorais-dev <guilherme_morais14@hotmail.com>
2023-09-13 14:00:26 -0300

    rotas delete and put

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -51,5 +51,34 @@ router.post("/user", checkJwt, async (req, res) => {
   res.status(200).json({ msg: " user created sucessfully" });
 });
 
+router.put("/user", checkJwt, async(req, res)=>{
+  const upatedeUser =  await userService(req.params.id);
+  const updatedUser = req.body;
+  const existingUser = users.find((user) => {
+    return (
+      (user.name === updatedUserData.name ||
+        user.email === updatedUserData.email) &&
+      user.id !== userId
+      
+    );
+    
+  });
+  if (existingUser) {
+    return res.status(400).json({ error: 'Name, ID or email already exist' });
+  }
+  const searchUser = userValidationExists.getFindById((user) => user.id===serId);
+})
+
+router.delete("/id", checkJwt, async(req, res)=>{
+  const deleteId = req.body(deleteId);
+  if (searchUser!= res.status(404).json)({ error: 'User not found' });
+   
+   
+
+
+
+})
+
+
 
 module.exports = router;
