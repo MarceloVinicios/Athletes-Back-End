94a84f05ed28b7e7279918c570f43252896d0bc7
Luiz Felipe <luizfelipealvesdesouza706@gmail.com>
2023-09-15 21:14:00 -0300

    router fulls

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -3,21 +3,19 @@ const router = express.Router();
 const userService = require("../services/userService");
 const checkJwt = require("../middleware/authToken");
 const UserModel = require("../models/UserModel");
-
+//Guiherme - rota de login.
 router.get("/user/token", checkJwt, async (req, res) => {
   try {
     const accessToken = req.headers.authorization.split(" ")[1];
     const responseCreate = await userService.acessToken(accessToken);
-    req.user = responseCreate.data;
-
-    res.status(200).json({ msg: "Acess token successfully" });
+    
   } catch (error) {
     res
       .status(500)
-      .json({ error: "Error getting token", message: error.message });
+      .json({ });
   }
 });
-
+//Guilherme - rota de postar.
 router.post("/user", checkJwt, async (req, res) => {
   const accessToken = req.headers.authorization.split(" ")[1];
   const responseCreate = await userService.acessToken(accessToken);
@@ -30,10 +28,10 @@ router.post("/user", checkJwt, async (req, res) => {
       .json({ error: userValidationExists.err, msg: userValidationExists.msg });
   }
 
-  if (userValidationExists.response.length  > 0) {
+  if (userValidationExists.response.length > 0) {
     return res.status(400).json({ msg: " user already registered " });
   }
-  
+
   const resultCreateUser = await UserModel.create(
     req.user.sub,
     req.user.email,
@@ -48,35 +46,35 @@ router.post("/user", checkJwt, async (req, res) => {
 
   res.status(201).json({ msg: " user created sucessfully" });
 });
+//Guilherme - rota de atualizar.
+router.put("/user", checkJwt, async (req, res) => {
+  try {
+    const { name, picture } = req.body;
+    const { sub } = req.user;
+    const upatedeUser = await userService.updateUser(sub, name, picture);
 
-router.put("/user", checkJwt, async(req, res)=>{
-  const upatedeUser =  await userService(req.params.id);
-  const updatedUser = req.body;
-  const existingUser = users.find((user) => {
-    return (
-      (user.name === updatedUserData.name ||
-        user.email === updatedUserData.email) &&
-      user.id !== userId
-      
-    );
-    
-  });
-  if (existingUser) {
-    return res.status(400).json({ error: 'Name, ID or email already exist' });
+    res.status(upatedeUser.statusCode).json(upatedeUser.response);
+  } catch (error) {
+    res
+      .status(500)
+      .json({ error: "Error update publication", message: error.message });
   }
-  const searchUser = userValidationExists.getFindById((user) => user.id===serId);
-})
+});
+//Guilherme - rota de deletar.
+router.delete("/user/:id?", checkJwt, async (req, res) => {
+  try{  
+    const {id} = req.params;
+    const deleteId = await userService.deleteUser(id, name, picture);
+    res.status(deleteId.statusCode).json(deleteId.response);
+  
 
-router.delete("/id", checkJwt, async(req, res)=>{
-  const deleteId = req.body(deleteId);
-  if (searchUser!= res.status(404).json)({ error: 'User not found' });
-   
-   
+  }catch (error) {
+    res
+      .status(500)
+      .json({ error: "Error update publication", message: error.message });
+  }
+  
 
-
-
-})
-
-
+});
 
 module.exports = router;
--- src/services/userService.js
+++ src/services/userService.js
@@ -1,33 +1,58 @@
 const axios = require("axios");
 const UserModel = require("../models/UserModel");
 
-
 class User_Service {
   async acessToken(token) {
-    return axios.get(
-      "https://dev-v6oinruanic8adgg.us.auth0.com/userinfo",
-      {
-        headers: {
-          Authorization: `Bearer ${token}`,
-        },
+    return axios.get("https://dev-v6oinruanic8adgg.us.auth0.com/userinfo", {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
+  }
+
+  async getAll() {}
+
+
+
+  async getUser() {}
+
+  async createUser() {}
+
+  async updateUser(id, name, picture) {
+    if (!name || !picture) {
+      return { statusCode: 400, response: "namme or picture undefined" };
+    }
+    const uptadeModel = await UserModel.update(id, name, picture);
+    if (!uptadeModel.status) {
+      return { statusCode: 500, response: uptadeModel.err };
+    }
+    return { statusCode: 200, response: "uptade successfully" };
+  }
+
+  async deleteUser(id ) {
+    try {
+      const searchUser = await UserModel.getFindById(id);
+      if (!searchUser.status) {
+      return { statusCode: 500, response: searchUser.err };
       }
-    );
-  };
 
-  async getAll(){
-  };
+      if(!searchUser.response.length){
+        return { statusCode: 404, response:"user not found for uptade" };
+      }
+      
+      const deleteOneUser = await UserModel.delete(id);
+      if(!deleteOneUser.status) {
+        return { statusCode: 500, response: deleteOneUser.err };
 
-  async getUser(){
-  };
+      }
 
-  async createUser(){
-  };
+      return { statusCode: 200, response: "delete user successfully"};
+    } catch (error) {
+      return { statusCode: 500, response: "failed to delete user" };
+    
+    }
 
-  async updateUser(){
-  };
-
-  async deleteUser(){
-  };
-};
+  }
+}
 
 module.exports = new User_Service();
