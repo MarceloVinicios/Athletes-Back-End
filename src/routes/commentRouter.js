2ab990b38619ac39c48a61e38d74574748c70c29
Marcelo Vinicios <marcelo1.dev@gmail.com>
2023-09-17 00:14:48 -0300

    <BA-10> rota delete comment concluida

--- src/routes/commentRouter.js
+++ src/routes/commentRouter.js
@@ -4,4 +4,17 @@ const router = express.Router();
 router.post("/comment", async (req, res) => {
 });
 
+router.delete("/comment/:id", async (req, res) => {
+  try {
+    const responseDeletePublication = await commentService.delete(req.params.id);
+
+    res.status(responseDeletePublication.status)
+      .json({response: responseDeletePublication.response})
+
+  } catch (error) {
+      res.status(500)
+        .json({ error: "Error delete publication", message: error.message })
+  }
+})
+
 module.exports = router;
--- src/services/commentService.js
+++ src/services/commentService.js
@@ -10,8 +10,30 @@ class Comment_service {
   async updateComment(){
   };
 
-  async deleteUserComment(){
+  async deleteUserComment(id){
+    if(isNaN(id)){
+      return {statusCode: 500, message:"Invalid id"};
+    };
+
+    const getComment = await CommentModel.findById(id);
+      if(!getComment.status) {
+        return {statusCode: 500, response: getComment.err};
+      }
+
+      if(!getComment.response.length) {
+        return {statusCode: 404, response:"Comment not found"};
+      }
+
+      const deleteComment = await CommentModel.delete(id)
+        if(!deleteComment.status) {
+          return {statusCode: 500, response: deleteComment.err};
+        }
+
+        return {statusCode: 200, response: "Comment deleted successfully"};
+      } catch (error) {
+        return {statusCode: 500, response: "Failed to delete comment"};
+      };
   };
-};
+
 
 module.exports = new Comment_service();
